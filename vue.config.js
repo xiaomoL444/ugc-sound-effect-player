840929f6b82e60befb6f6743cf8e677578942738
xiaomoL444 <2802273114@qq.com>
2025-12-30 16:18:56 +0800

    修改了publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'/SoundEffectPlayer',
+  publicPath:'/UGCSoundEffectPlayer',
   transpileDependencies: true
 })
